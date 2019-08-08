f36216e6ca87b733baf660ad9fee651838ffafe5
Lucas Massucci <lucas.massucci@outlook.com>
2019-08-08 19:07:11 -0300

    Migration to Eclipse Idle and javadoc creation

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"ByteBank","l":"br.com.bytebank.bank.model"},{"m":"ByteBank","l":"br.com.bytebank.bank.test"}]
---
+++ src/module-info.java
@@ -0,0 +1,4 @@
+module ByteBank {
+	exports br.com.bytebank.bank.test;
+	exports br.com.bytebank.bank.model;
+}
